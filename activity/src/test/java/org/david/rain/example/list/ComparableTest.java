c86a3a23e1ffe4eb1c8f3ec8c1ad5ba1fba2dd70
david3424 <david.3424@gmail.com>
2014-10-22 23:19:22 +0800

    commit activity modual

---
+++ activity/src/test/java/org/david/rain/example/list/ComparableTest.java
@@ -0,0 +1,37 @@
+package org.david.rain.example.list;
+
+import org.junit.Test;
+import org.apache.commons.lang.builder.*;
+
+/**
+ * Created by zhangxingang on 14-10-22.
+ */
+public class ComparableTest {
+    CompareToBuilder compareToBuilder;
+    @Test
+    public void ComparableTest(){
+
+    }
+
+    enum Position{
+        Boss,Manager,Staff
+    }
+
+    private static class Employee implements Comparable<Employee>{
+         int id;
+         String name;
+        Position positon;
+
+         Employee(int _id,String _name,Position _position){
+            this.id = _id;
+             this.name = _name;
+             this.positon = _position;
+        }
+
+        @Override
+        public int compareTo(Employee o) {
+            return new CompareToBuilder().a;
+        }
+    }
+
+}
---
+++ activity/src/test/java/org/david/rain/example/list/SubListTest.java
@@ -0,0 +1,23 @@
+package org.david.rain.example.list;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by zhangxingang on 14-10-22.
+ */
+public class SubListTest {
+
+
+
+    @Test
+            public void subListTest(){
+    List<Integer> initData = Collections.nCopies(100,0);
+    ArrayList<Integer> list = new ArrayList<>(initData);
+        list.subList(20,30).clear();
+        System.out.println("list is " + list);
+    }
+}
