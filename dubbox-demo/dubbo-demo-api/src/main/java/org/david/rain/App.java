dde1f8a4523d3cff02bdc31c6384c6f685a5bdb1
david3424 <david.3424@gmail.com>
2015-05-20 18:58:20 +0800

    add pay modual

---
+++ dubbox-demo/dubbo-demo-api/src/main/java/org/david/rain/App.java
@@ -0,0 +1,13 @@
+package org.david.rain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
