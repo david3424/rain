c844e1c9291bf4e1f55e570bfaf480408e50d610
xdw9486 <david.3424@gmail.com>
2017-12-21 11:04:42 +0800

    add zuul

--- crm-cloud/zuul/src/main/java/com/noah/crm/cloud/zuul/ZuulApplication.java
+++ crm-cloud/zuul/src/main/java/com/noah/crm/cloud/zuul/ZuulApplication.java
@@ -11,13 +11,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-public class Application {
+public class ZuulApplication {
 
 
 
     public static void main(String[] args) {
 
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
     }
 
 }
